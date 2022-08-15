dac254781b7d0f797379dda87d20bce8f2984359
Berrgen <sergeu.css@gmail.com>
2022-08-15 10:54:06 +0300

    add Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,21 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+        boolean comparison = bean1 == bean2;
+        boolean comparison1 = beanCat1 == beanCat2;
+
+        System.out.println(comparison);
+        System.out.println(comparison1);
+
+        System.out.println(bean1.getMessage());
+        System.out.println(beanCat1.getCat());
     }
 }
